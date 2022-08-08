3b275366591cff071ffe52d4db28ee4d5063091f
KillerMR <killermbjustthink@gmail.com>
2022-08-08 11:59:51 +0300

    Example(2235): Add Two Integers

--- LeetCodeCpp/LeetCodeCpp/LeetCodeTraining.cpp
+++ LeetCodeCpp/LeetCodeCpp/LeetCodeTraining.cpp
@@ -7,21 +7,22 @@ using std::cin;
 
 class Solution {
 public:
-    vector<int> buildArray(vector<int>& nums) {
-        vector<int> res;
-        for (int i = 0; i < nums.size(); i++) {
-            res.push_back(nums[nums[i]]);
-        }
-        return res;
+    int sum(int num1, int num2) {
+        return num1 + num2;
     }
 };
 
 int main() {
-    vector<int> nums = { 0, 2, 1, 5, 3, 4 };
+    int num1 = 5;
+    int num2 = 5;
+    Solution sol;
+    cout << sol.sum(num1, num2);
+    return 0;
+    // Для виведення векторів
+    /*vector<int> nums = { 0, 2, 1, 5, 3, 4 };
     Solution sol = Solution();
     vector<int> res = sol.buildArray(nums);
     for (int i = 0; i < res.size(); i++) {
         cout << res[i] << std::endl;
-    }
-    return 0;
+    }*/
 }
