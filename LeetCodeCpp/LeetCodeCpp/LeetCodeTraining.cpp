297ab30e9f771dc315e2cc10db18f553a45929c3
KillerMR <killermbjustthink@gmail.com>
2022-08-08 11:54:18 +0300

    Example(1920): Build Array from Permutation

--- LeetCodeCpp/LeetCodeCpp/LeetCodeTraining.cpp
+++ LeetCodeCpp/LeetCodeCpp/LeetCodeTraining.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+#include <vector>
+
+using std::vector;
+using std::cout;
+using std::cin;
+
+class Solution {
+public:
+    vector<int> buildArray(vector<int>& nums) {
+        vector<int> res;
+        for (int i = 0; i < nums.size(); i++) {
+            res.push_back(nums[nums[i]]);
+        }
+        return res;
+    }
+};
 
 int main() {
-
-	return 0;
+    vector<int> nums = { 0, 2, 1, 5, 3, 4 };
+    Solution sol = Solution();
+    vector<int> res = sol.buildArray(nums);
+    for (int i = 0; i < res.size(); i++) {
+        cout << res[i] << std::endl;
+    }
+    return 0;
 }
